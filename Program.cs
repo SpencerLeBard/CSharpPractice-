a8f8aedc9ee4eb87b24743542b3db488fcfa6a40
SpencerLeBard <slebard@gmail.com>
2020-12-01 10:07:02 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace helloworld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter Number");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Enter Number");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(num1 + num2);
+    }
+  }
+}
