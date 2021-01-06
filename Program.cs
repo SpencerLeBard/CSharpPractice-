89061274c8291383e23c587cb0183329abaf6159
SpencerLeBard <slebard@gmail.com>
2021-01-06 10:01:52 -0700

    razor pages

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace helloworld
 {
-  class Program
-  {
-    static void Main(string[] args)
+    public class Program
     {
-      SayHi("Mike");
-      Console.ReadLine();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-    static void SayHi(string name)
-    {
-      Console.WriteLine("hello " + name);
-    }
-  }
 }
